898cea9c888a1bcb5a333ef66d33f8b223463773
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-14 11:48:07 -0600

    added markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,35 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-    
-  
+    ## Descritpion
+    ${data.description}
+
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## License
+    ${data.license}
+
+    ## Contributing
+    ${data.contribution}
+
+    ## Tests
+    ${data.tests}
+
+    ## Questions
+    &copy; ${data.name}, ${data.github}, ${data.email}
   `;
 }
 
