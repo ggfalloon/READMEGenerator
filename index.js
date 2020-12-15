7ba823f89bc816ba7d72f210c33c3ccf94227842
Gabriele Falloon <ggfalloon@gmail.com>
2020-12-15 11:08:54 -0600

    functioning questions

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const utils = require('/utils/.generateMarkdown.js');
+const utils = require('./utils/generateMarkdown.js');
 
 // array of questions for user
-const questions = () =>
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Title of your project?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Write your description.',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Write your installation instructions.',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Write your usage information.',
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Write your contribution guidelines.',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Write your testing instructions.',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Choose a license.',
-            choices: ['MIT', 'BSD', 'Apache', 'GNU']
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter the email address you want to display.',
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter your name the way you want to display it.',
-        },
-    ])
+// const questions = () =>
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write your description.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write your installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write your usage information.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Write your contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write your testing instructions.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license.',
+        choices: ['MIT', 'BSD', 'Apache', 'GNU']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the email address you want to display.',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name the way you want to display it.',
+    },
+])
 
-        .then((data) => {
-            // function to write README file
+    .then((data) => {
+        // function to write README file
 
-            fs.writeToFile(README.md, data), (err) => {
-                err ? console.log(err) : console.log('Success!');
+        fs.writeFile("README.md", utils, data), (err) => {
+            err ? console.log(err) : console.log('Success!');
 
-            }
-        });
+        }
+    });
 
 // function to initialize program
 function init() {
